0fcffeeafafd2e2eed9525a276bffa8228ba0b8c
yuntian <944610721@qq.com>
2019-01-04 23:06:25 +0800

    aspectj-plugin 1.0插件发布

--- app/src/main/java/com/yuntian/androidaop/MainActivity.java
+++ app/src/main/java/com/yuntian/androidaop/MainActivity.java
@@ -3,11 +3,26 @@ package com.yuntian.androidaop;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.yuntian.aoplib.annotation.DebugTrace;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.tv).setOnClickListener(v->{
+            test();
+        });
     }
+
+    @DebugTrace
+    public void  test(){
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
